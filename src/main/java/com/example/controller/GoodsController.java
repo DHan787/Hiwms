73b02f3ab57e707c981bd14b547cd0ddd6f7272c
fayeh7 <582258127@qq.com>
2022-06-30 20:48:05 +0800

    货物的显示和删除

--- src/main/java/com/example/controller/GoodsController.java
+++ src/main/java/com/example/controller/GoodsController.java
@@ -16,16 +16,37 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    /**
+     * 获取全部货品
+     * @return
+     */
     @GetMapping//访问方式
     public List<Goods> getAll() {
-        log.info("goods获取的数据，{}",goodsService.list());
-        System.out.println(goodsService.getById(1));
+        //log.info("goods获取的数据，{}",goodsService.list());
         System.out.println("used");
         return goodsService.list();
     }
+
+    /**
+     * 根据id获取货物
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public Goods getById(@PathVariable int id) {
         return goodsService.getById(id);
+    }
+
+    /**
+     * 删除货物
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable int id){
+
+        return goodsService.removeById(id);
 
     }
+
 }
