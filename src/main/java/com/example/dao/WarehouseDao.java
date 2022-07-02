4c5c7d37b584d80a832cf17de8af1e28058e4d60
chenhao0407 <929574742@qq.com>
2022-07-02 14:42:58 +0800

    新增所有warehouse文件

---
+++ src/main/java/com/example/dao/WarehouseDao.java
@@ -0,0 +1,9 @@
+package com.example.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.domain.Warehouse;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+
+public interface WarehouseDao extends BaseMapper<Warehouse> {
+}
