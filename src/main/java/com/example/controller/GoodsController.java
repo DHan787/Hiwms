1afd6a0825f88d27ff9ccdefebfb93cb939e09c7
HanassssFish <sayeer.alicia@gmail.com>
2022-07-08 16:45:19 +0800

    货物页面的excel导入导出

--- src/main/java/com/example/controller/GoodsController.java
+++ src/main/java/com/example/controller/GoodsController.java
@@ -1,12 +1,22 @@
 package com.example.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.example.dao.GoodsDao;
 import com.example.domain.Goods;
 import com.example.service.GoodsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 @Slf4j
 @CrossOrigin
@@ -77,5 +87,72 @@ public class GoodsController {
     public List<Goods> getAllList(@RequestParam String goodsName){
         System.out.println(goodsName);
         return goodsDao.selectGoodsName("%"+goodsName+"%");
+    }
+
+    /**
+     * excel导出
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("/exportExcel")
+    public void exportExcel(HttpServletResponse response) throws Exception {
+        //查询所有数据
+        List<Goods> list = goodsService.list();
+        //在内存操作，写出到浏览器，从浏览器下载
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        //自定义标题名
+        writer.addHeaderAlias("goodsName", "货物名称");
+        writer.addHeaderAlias("goodsType", "货物类别");
+        writer.addHeaderAlias("goodsSize", "货物尺寸");
+        writer.addHeaderAlias("goodsValue", "货物价格");
+
+        //一次性写出list内的对象到excel，使用默认格式，强制输出标题
+        writer.write(list,true);
+
+        //设置浏览器响应格式
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("货物信息","UTF-8");
+        response.setHeader("Content-Disposition","attachment;filename="+fileName+".xlsx");
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        writer.flush(outputStream,true);
+
+        //关闭流
+        outputStream.close();
+        writer.close();
+    }
+
+
+    /**
+     * excel导入
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/importExcel")
+    public Boolean importExcel(MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+
+        //方式1：通过JavaBean的方式读取excel内的对象，但是要求表头必须市英文，和JavaBean属性对应
+//        List<User> users = reader.readAll(User.class);
+
+        //方式二：忽略表头中文，直接获取表格数据
+        List<List<Object>> list = reader.read(1);
+        List<Goods> good = CollUtil.newArrayList();
+
+        for(List<Object> row:list){
+            Goods goods =new Goods();
+            goods.setGoodsName(row.get(1).toString());
+            goods.setGoodsType(row.get(2).toString());
+            goods.setGoodsSize(row.get(3).toString());
+            goods.setGoodsValue(row.get(4).toString());
+
+            good.add(goods);
+        }
+
+        //将excel导入的数据保存到数据库
+        goodsService.saveBatch(good);
+        return true;
     }
 }
