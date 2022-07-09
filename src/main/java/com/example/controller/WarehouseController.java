aa2948ef4f34b9efc79017f30997c94eaa22f49f
Ginger1221 <877653250@qq.com>
2022-07-09 16:55:45 +0800

    little changes

--- src/main/java/com/example/controller/WarehouseController.java
+++ src/main/java/com/example/controller/WarehouseController.java
@@ -21,7 +21,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/warehouse")
-public class WarehosueController {
+public class WarehouseController {
     @Autowired
     private WarehouseService warehouseService;
 
