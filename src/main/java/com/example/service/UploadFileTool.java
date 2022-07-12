84d63fa51f6d69017f454dd9d74449f2a6ae2dab
Ginger1221 <877653250@qq.com>
2022-07-12 18:05:27 +0800

    文件上传后端V2.1 无报错 其他功能可用

--- src/main/java/com/example/service/UploadFileTool.java
+++ src/main/java/com/example/service/UploadFileTool.java
@@ -1,21 +1,21 @@
-//package com.example.service;
-//
-//import com.example.domain.UploadFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-///**
-// * 文件上传工具接口
-// * @author ginger
-// */
-//public interface UploadFileTool {
-//    /**
-//     * 文件上传
-//     *
-//     * @param uploader  上传人
-//     * @param realName  文件实际名称
-//     * @param multipartFile 文件
-//     * @return 上传信息
-//     */
-//    UploadFile upload(String uploader, String realName, MultipartFile multipartFile);
-//}
-//
+package com.example.service;
+
+import com.example.domain.UploadFile;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件上传工具接口
+ * @author ginger
+ */
+public interface UploadFileTool {
+    /**
+     * 文件上传
+     *
+     * @param uploader  上传人
+     * @param realName  文件实际名称
+     * @param multipartFile 文件
+     * @return 上传信息
+     */
+    UploadFile upload(String uploader, String realName, MultipartFile multipartFile);
+}
+
