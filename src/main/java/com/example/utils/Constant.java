866e71fc975ada02d231023dba115f5f36722d16
Ginger1221 <877653250@qq.com>
2022-07-12 16:39:36 +0800

    文件上传后端V1.0

---
+++ src/main/java/com/example/utils/Constant.java
@@ -0,0 +1,27 @@
+package com.example.utils;
+
+/**
+ * @author ginger
+ */
+public class Constant {
+
+        /**
+         * 定义Redis缓存默认过期时间
+         */
+        public static final int CACHE_TIMEOUT_HOUR=2;
+
+
+        /**
+         * 定义unknown字串串的常量
+         */
+        public  static final String UNKNOWN = "unknown";
+
+        /**
+         * 定义MB的计算常量
+         */
+        public static final int MB = 1024 * 1024;
+        /**
+         * 定义KB的计算常量
+         */
+        public static final int KB = 1024;
+}
